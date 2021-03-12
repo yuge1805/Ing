666c52320ab4497cbafba54525bc114ad73c6b72
yuge <yuge1805@yeah.net>
2021-03-12 10:00:07 +0800

    fix: 修改架构

---
+++ yuge-cloud/web-spring-cloud-starter/src/main/java/com/yuge/cloud/web/WebConfiguration.java
@@ -0,0 +1,11 @@
+package com.yuge.cloud.web;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: yuge
+ * @date: 2021-03-11
+ **/
+@Configuration
+public class WebConfiguration {
+}
