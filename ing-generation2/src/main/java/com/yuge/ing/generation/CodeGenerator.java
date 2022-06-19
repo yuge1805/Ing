cff6b83761d7f58c72ba7d75300b3d01ed0ef006
yuge <yuge1805@yeah.net>
2022-06-19 21:25:21 +0800

    feat： 备份修改提交

--- ing-generation2/src/main/java/com/yuge/ing/generation/CodeGenerator.java
+++ ing-generation2/src/main/java/com/yuge/ing/generation/CodeGenerator.java
@@ -3,16 +3,26 @@ package com.yuge.ing.generation;
 public class CodeGenerator {
     public static void main(String[] args) {
         DataSourceInfo info = new DataSourceInfo(
-                "jdbc:mysql://localhost:3306/ing_seata_order?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2B8",
+                "jdbc:mysql://localhost:3306/ing_seata_store?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2B8",
                 "root",
                 "root");
+//        CodeGenerationUtil.generation(
+//                "com.yuge.ing.seata.order",
+//                info,
+//                "yuge",
+//                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-order\\order-server\\src\\main\\java",
+//                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-order\\order-server\\src\\main\\resources\\mapper",
+//                "biz_",
+//                "biz_order");
+
         CodeGenerationUtil.generation(
-                "com.yuge.ing.seata.order",
+                "com.yuge.ing.seata.store",
                 info,
                 "yuge",
-                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-order\\src\\main\\java",
-                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-order\\src\\main\\resources\\mapper",
+                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-store\\store-server\\src\\main\\java",
+                "E:\\Program Files\\Workspace\\My-Workspace\\ing-seata\\seata-store\\store-server\\src\\main\\resources\\mapper",
                 "biz_",
-                "biz_order");
+                "biz_store");
+
     }
 }
